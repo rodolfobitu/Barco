065225eb5d9c5502800f3999f91103ae51001db6
Guilherme Souza <sitegui@sitegui.com.br>
2015-04-17 22:29:04 -0300

    serial com ponto h arrumado

--- headers/serialcom.h
+++ headers/serialcom.h
@@ -28,4 +28,24 @@ void sc_init(void);
 /* ************************************************ */
 void sc_start(void);
 
+
+/* ************************************************ */
+/* Method name: 	   sc_sendBuffer		   		*/
+/* Method description: Sends buffer using the serial*/
+/* 						communication.				*/
+/* Input params:	   char* cBuf					*/
+/* Outpu params:	   n/a 							*/
+/* ************************************************ */
+void sc_sendBuffer(char* cBuf);
+
+/* ************************************************ */
+/* Method name: 	   sc_receiveBuffer		   		*/
+/* Method description: Receive a buffer using serial*/
+/* 						communication.				*/
+/* Input params:	   char* cBuf					*/
+/* Outpu params:	   n/a 							*/
+/* ************************************************ */
+void sc_receiveBuffer(char* cBuf);
+
+
 #endif /* SERIALCOM_H */
